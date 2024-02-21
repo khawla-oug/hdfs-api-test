01738b5ffc2b920135bb09e2d4c59ecaac6458bc
ZarhouniSaad <112633625+ZarhouniSaad@users.noreply.github.com>
2024-02-21 14:01:14 +0100

    commit

--- src/main/java/ma/emsi/lpri/HDFSFileRetrieval.java
+++ src/main/java/ma/emsi/lpri/HDFSFileRetrieval.java
@@ -1,28 +1,30 @@
 package ma.emsi.lpri;
+
 import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 
 public class HDFSFileRetrieval {
 
     public static void main(String[] args) throws IOException {
         String hdfsUri = "http://172.20.43.105:9870/webhdfs/v1";
-        String hdfsFilePath = "/download.jpg?op=OPEN";
-        String localFilePath = "src/main/resources/photo.jpg";
+        String hdfsFilePath = "/cv.pdf";
+        String localFilePath = "src/main/resources/cv.pdf";
 
         retrieveFileFromHDFS(hdfsUri, hdfsFilePath, localFilePath);
     }
 
     public static void retrieveFileFromHDFS(String hdfsUri, String hdfsFilePath, String localFilePath)
             throws IOException {
-        URL url = new URL(hdfsUri + hdfsFilePath);
-        URLConnection connection = url.openConnection();
+        URL url = new URL(hdfsUri + hdfsFilePath + "?op=OPEN");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-        try (InputStream inStream = new BufferedInputStream(connection.getInputStream());
-             FileOutputStream outStream = new FileOutputStream(localFilePath)) {
+        try {
+            InputStream inStream = new BufferedInputStream(connection.getInputStream());
+            FileOutputStream outStream = new FileOutputStream(localFilePath);
 
             byte[] buffer = new byte[4096];
             int bytesRead;
@@ -33,7 +35,12 @@ public class HDFSFileRetrieval {
 
             System.out.println("File retrieved successfully!");
         } catch (IOException e) {
-            System.err.println("Error retrieving file: " + e.getMessage());
+            int responseCode = connection.getResponseCode();
+            String responseMessage = connection.getResponseMessage();
+
+            System.err.println("Error retrieving file. Response code: " + responseCode + ", Message: " + responseMessage);
+        } finally {
+            connection.disconnect();
         }
     }
 }
